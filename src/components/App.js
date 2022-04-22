c97579a5ecb856284595ae1a063e6672bd140130
Bliqui <ronlyred@gmail.com>
2022-04-23 01:57:46 +0200

    Update Landing component

---
+++ src/components/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class App extends React.Component {
+    render() {
+        return(
+            {/*app code will be here*/}
+        )
+    }
+}
--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,29 +1,52 @@
 import React from "react";
 import cafes from "../sample-cafes";
+import * as Path from "path";
 
 class Landing extends React.Component {
-    render() {
+    state = {
+        display: false,
+        title: '',
+        url: ''
+    };
 
+    displayList = () => {
+        const {display} = this.state;
+        this.setState({display: !display});
+    };
+
+    getTitle = (cafe) => {
+        const { title, url } = cafe;
+        this.setState({title: title, url: url, display: false})
+    };
+
+    goToCafe = () => {
+        console.log('go to cafe')
+    };
+
+    render() {
         let cafesList = cafes
-            .map((cafe) => <li key={cafe.id}>{ cafe.title }</li>);
+            .map((cafe) => <li onClick={() => this.getTitle(cafe)} key={cafe.id}>{cafe.title}</li>);
 
         return (
             <div className={"restaurant_select"}>
                 <div className={'restaurant_select_top'}>
-                    <div className={'restaurant_select_top-header font-effect-outline'}>Choose Caffe</div>
-
+                    <div onClick={this.displayList} className={'restaurant_select_top-header font-effect-outline'}>
+                        {this.state.title ? this.state.title : 'Choose Caffe'}
+                    </div>
                     <div className={'arrow_picker'}>
                         <div className={'arrow_picker-up'}></div>
                         <div className={'arrow_picker-down'}></div>
                     </div>
                 </div>
 
-                <div className={'restaurant_select_bottom'}>
+                {this.state.display ? <div className={'restaurant_select_bottom'}>
                     <ul>
                         {cafesList}
                     </ul>
-                </div>
-                <button>Go to caffe menu</button>
+                </div> : null}
+                {this.state.title && !this.state.display ?
+                    <button onClick={this.goToCafe}>Go to caffe menu</button>
+                    : null}
             </div>
         );
     };
