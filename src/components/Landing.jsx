e8b52aa7f1c25e05d162e5d88d82093f13adff9e
Bliqui <ronlyred@gmail.com>
2022-04-23 01:29:52 +0200

    Update Landing component

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,8 +1,31 @@
 import React from "react";
+import cafes from "../sample-cafes";
 
 class Landing extends React.Component {
-    render () {
-        return <p>Hi</p>;
+    render() {
+
+        let cafesList = cafes
+            .map((cafe) => <li key={cafe.id}>{ cafe.title }</li>);
+
+        return (
+            <div className={"restaurant_select"}>
+                <div className={'restaurant_select_top'}>
+                    <div className={'restaurant_select_top-header font-effect-outline'}>Choose Caffe</div>
+
+                    <div className={'arrow_picker'}>
+                        <div className={'arrow_picker-up'}></div>
+                        <div className={'arrow_picker-down'}></div>
+                    </div>
+                </div>
+
+                <div className={'restaurant_select_bottom'}>
+                    <ul>
+                        {cafesList}
+                    </ul>
+                </div>
+                <button>Go to caffe menu</button>
+            </div>
+        );
     };
 };
 
