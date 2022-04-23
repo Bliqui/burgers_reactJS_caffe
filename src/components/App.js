851cbe5f72ea4164373c7533871b63e6406f5551
Bliqui <ronlyred@gmail.com>
2022-04-23 02:44:12 +0200

    Add AdminMenu component

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,17 @@
 import React from "react";
 import Header from './Header';
 import Order from "./Order";
+import AdminMenu from "./AdminMenu";
 
 class App extends React.Component {
     render() {
         return(
             <div className={'burger-paradise'}>
-                <div className={'menu-burger'}>
+                <div className={'menu'}>
                     <Header />
                 </div>
                 <Order/>
+                <AdminMenu />
             </div>
         )
     }
